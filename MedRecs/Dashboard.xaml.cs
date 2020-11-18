7c93b9e7e138da94ed607c0d415458cf03d2bc02
AshrafTaifour <60125273+AshrafTaifour@users.noreply.github.com>
2020-11-17 23:08:54 -0500

    added clock to the dashboard

--- MedRecs/Dashboard.xaml.cs
+++ MedRecs/Dashboard.xaml.cs
@@ -7,11 +7,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using LoginPageTest;
+
 
 namespace MedRecs
 {
@@ -20,19 +22,39 @@ namespace MedRecs
     /// </summary>
     public partial class Dashboard : Window
     {
-        private HEALTHCARE_PERSONNEL user;
+        private HEALTHCARE_PERSONNEL user { set; get; }
+
+        //private 
+
         public Dashboard(HEALTHCARE_PERSONNEL user)
         {
             InitializeComponent();
             this.user = user;
             username.Content = this.user.email;
-            date.Content = DateTime.Today;
+            date.Content = DateTime.Now;
+            Dashboard_Load();
         }
 
         private void LogOut_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
+
+        private void Dashboard_Load()
+        {
+            Timer timer = new Timer();
+            timer.Interval = (10 * 1000); // 10 secs
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            date.Content = DateTime.Now;
+        }
+        
+
 
     }
 }
