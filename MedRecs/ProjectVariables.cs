751c61838c86264f0e8ef370ebc3de6719c38b14
Santo (Spencer) Briguglio <brigugls@uwindsor.ca>
2020-11-20 18:41:06 -0500

    Added static class, ProjectVariables.cs

---
+++ MedRecs/ProjectVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedRecs
+{
+    static class ProjectVariables
+    {
+         static string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ssbri\source\repos\AshrafTaifour\MedRecs\MedRecs\MedicalDatabase.mdf";
+    }
+}
