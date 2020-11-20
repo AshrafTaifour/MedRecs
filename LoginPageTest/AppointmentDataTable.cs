f3f4cac91b32c90b6b2b190b759dce7daa38a635
Santo (Spencer) Briguglio <brigugls@uwindsor.ca>
2020-11-19 22:13:41 -0500

    Updated Database APPOINTMENT table to have date and time fields

---
+++ LoginPageTest/AppointmentDataTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace LoginPageTest
+{
+    class AppointmentDataTable
+    {
+        private DataTable aptDT;
+
+        public AppointmentDataTable()
+        {
+            DataSet aptDataSet = new DataSet("AppointmentTimes");
+
+        }
+
+        public AppointmentDataTable(/*Date*/)
+        {
+
+        }
+
+        public AppointmentDataTable(/*StartDay, EndDay*/)
+        {
+
+        }
+    }
+}
