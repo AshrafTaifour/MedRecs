6efb7af2923f785b1381fdd26c256dd977ac4d38
AshrafTaifour <60125273+AshrafTaifour@users.noreply.github.com>
2020-11-20 22:58:04 -0500

    Revert "Final final commit"
    This reverts commit 0ce5880f73be6f4316e4c135d6408ea519e03a30.

--- MedRecs/Properties/Settings.Designer.cs
+++ MedRecs/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace MedRecs.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -28,9 +28,9 @@ namespace MedRecs.Properties {
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\MedicalDataba" +
             "se.mdf;Integrated Security=True")]
-        public string MedicalDatabaseConnection {
+        public string MedicalDatabaseConnectionString {
             get {
-                return ((string)(this["MedicalDatabaseConnection"]));
+                return ((string)(this["MedicalDatabaseConnectionString"]));
             }
         }
     }
